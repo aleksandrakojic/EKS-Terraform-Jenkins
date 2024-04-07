d114e18e1903ceba91b52fb9497d983a9821d145
aleks <alexandra.kojic@gmail.com>
2024-04-07 22:07:32 +0200

    fix jenkins file

--- provider.tf
+++ provider.tf
@@ -6,9 +6,8 @@ provider "aws" {
 # configure backend
 terraform {
   backend "s3" {
-    bucket         = "terraform"
-    key            = "eks.terraform.tfstate"
+    bucket         = "ehop-bucket"
+    key            = "eks/terraform.tfstate"
     region         = "eu-west-3"
-    dynamodb_table = "terraform-state-lock-dynamodb"
   }
 }
